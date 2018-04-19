17a712ebb990d25438db7e0e6dff67cf17060c9e
Thomas Renaud <trenaud@stva.com>
2018-04-19 17:40:38 +0200

    Modif Activ 1

--- Activite_1/js/main.js
+++ Activite_1/js/main.js
@@ -1,6 +1,53 @@
 $(function () {
-    $('li.impair').fadeOut('slow', function () {
-        $(this).fadeIn('slow');
-    }).css('background','red').css('color','yellow');
+
+    // ############ Déclaration Variables ############
+
+    // ###############################################    
+
+    // ############ Déclaration function ############
+
+    var masquer = function (el) {
+        $(el).hide();
+    }
+
+    var visible = function (el) {
+        $(el).show();
+    }
+
+    // ############################################### 
     
+    // ############ Zone de test ############
+    
+    console.log();
+
+    // ############################################### 
+
+    masquer('#infoLabel');
+    masquer('#infoId');
+    masquer('#infoButton');
+    masquer('#infoValid');
+
+    $('button[id="label"]').click(function () {
+        visible('#infoLabel');
+        visible('#infoValid');
+        masquer('#infoId');
+        masquer('#infoButton');
+    });
+
+    $('button[id="zoneTxt"]').click(function () {
+        visible('#infoId');
+        visible('#infoValid');
+        masquer('#infoLabel');
+        masquer('#infoButton');
+    });
+
+    $('button[id="bouton"]').click(function () {
+        visible('#infoButton');
+        visible('#infoValid');
+        masquer('#infoLabel');
+        masquer('#infoId');
+    });
+
+
+
 });
