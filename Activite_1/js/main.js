3ffe15ed4525621a141992d254f68bdabe24a26d
Thomas Renaud <trenaud@stva.com>
2018-04-20 15:41:21 +0200

    Fin Activite 1

--- Activite_1/js/main.js
+++ Activite_1/js/main.js
@@ -2,6 +2,7 @@ $(function () {
 
     // ############ Déclaration Variables ############
 
+    var val;
     // ###############################################    
 
     // ############ Déclaration function ############
@@ -15,37 +16,68 @@ $(function () {
     }
 
     // ############################################### 
-    
+
     // ############ Zone de test ############
-    
-    console.log();
+
+
 
     // ############################################### 
 
     masquer('#infoLabel');
     masquer('#infoId');
     masquer('#infoButton');
-    masquer('#infoValid');
+    masquer('#ok');
 
     $('button[id="label"]').click(function () {
         visible('#infoLabel');
-        visible('#infoValid');
+        visible('#ok');
         masquer('#infoId');
         masquer('#infoButton');
     });
 
     $('button[id="zoneTxt"]').click(function () {
         visible('#infoId');
-        visible('#infoValid');
+        visible('#ok');
         masquer('#infoLabel');
         masquer('#infoButton');
     });
 
     $('button[id="bouton"]').click(function () {
         visible('#infoButton');
-        visible('#infoValid');
+        visible('#ok');
         masquer('#infoLabel');
         masquer('#infoId');
+    });
+
+    $('#ok').click(function () {
+
+            if ($('#inputInfoLabel').val() !== "") {
+                val = $('#inputInfoLabel').val();
+                $('#gauche').append ('<span>'+val+'</span>');
+                $('#gauche > span').css('margin', '15px');
+                $('#inputInfoLabel').val("");
+                masquer('#infoLabel');
+                masquer('#ok');
+            }
+            
+            else if ($('#inputInfoId').val() !== "") {
+                val = $('#inputInfoId').val();
+                $('#gauche').append('<input id="'+val+'" type="text" /><br/>');
+                $('#'+val).css('margin', '15px');
+                $('#inputInfoId').val("");
+                masquer('#infoId');
+                masquer('#ok');
+            }
+            
+            else if ($('#inputInfoButton').val() !== "") {
+                val = $('#inputInfoButton').val();
+                $('#gauche').append('<button>'+val+'</button>');
+                $('#gauche > button').css('margin', '15px');
+                $('#inputInfoButton').val("");
+                masquer('#infoButton');
+                masquer('#ok');
+            }
+        
     });
 
 
